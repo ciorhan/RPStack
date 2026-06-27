883d4ef45b6c51b0d564b132d6ab624031dff9d5
Ciorhan Florin <ciorhanflorin@gmail.com>
2026-06-27 04:13:00 +0300

    chore: remove root prototype — framework lives in resources/rpstack-*

--- server/bootstrap.lua
+++
@@ -1,9 +0,0 @@
-AddEventHandler('playerJoining', function()
-  local src = source
-  PlayerRegistry.add(src)
-end)
-
-AddEventHandler('playerDropped', function()
-  local src = source
-  PlayerRegistry.remove(src)
-end)
